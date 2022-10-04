8d337c534ade81115c1951d89f0619e95a4f5eed
Vincent Lee <vlee@vlee.me.uk>
2022-10-04 16:05:44 +0100

    attempt to fix CORS cookie bug

--- app/main.py
+++ app/main.py
@@ -15,6 +15,13 @@ system_variables = Config()  # load in config file
 
 debug = system_variables.debug
 
+origins = [
+    "http://localhost",
+    "file:///",
+    "http://localhost:63342",
+    "http://127.0.0.1:5500"
+]
+
 # Initialize logging
 logging.basicConfig(
     level=(
@@ -56,7 +63,8 @@ def create_app():
     # Add cors
     new_app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
+        allow_origins=origins,
+        # allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
