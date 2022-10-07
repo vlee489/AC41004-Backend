83f3d6f9ab5dc992be7d012925ec8132218496b7
alecavi <a.cavicchioli@dundee.ac.uk>
2022-10-07 12:15:46 +0100

    Hotfixed a misplaced function in the wrong file

--- app/database/__exceptions.py
+++ app/database/__exceptions.py
@@ -189,3 +189,30 @@ async def get_exception_by_date_account_id(self: 'DBConnector', account_id: Unio
         return return_list
     except InvalidId:
         return []
+
+
+async def add_exception(
+        self: 'DBConnector',
+        customer_id: Union[ObjectId, str],
+        rule_id: Union[ObjectId, str],
+        last_updated_by: Union[ObjectId, str],
+        exception_value: str,
+        justification: str,
+        review_date: datetime,
+        last_updated: datetime
+    ) -> ObjectId:
+    if type(customer_id) == str:
+        customer_id = ObjectId(customer_id)
+    if type(rule_id) == str:
+        rule_id = ObjectId(rule_id)
+    if type(last_updated_by) == str:
+        last_updated_by = ObjectId(last_updated_by)
+    return (await self._db.exceptions.insert_one({
+        "customer_id": customer_id,
+        "rule_id": rule_id,
+        "last_updated_by": last_updated_by,
+        "exception_value": exception_value,
+        "justification": justification,
+        "review_date": review_date,
+        "last_updated": last_updated
+    })).inserted_id
