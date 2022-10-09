6a42ab4dc6db764cf5b0cf107ddfbbab4422eebd
Vincent Lee <vlee@vlee.me.uk>
2022-10-10 00:09:50 +0100

    Starting code to get non-compliance audit

--- app/database/__nonComplianceAudit.py
+++ app/database/__nonComplianceAudit.py
@@ -24,3 +24,109 @@ async def get_non_compliance_audit_by_id(self: 'DBConnector', non_compliance_aud
             return NonComplianceAudit(result)
     except InvalidId:
         return None
+
+
+async def get_non_compliance_audit_by_resource_id(self: 'DBConnector', resource_id: Union[ObjectId, str]):
+    try:
+        if type(resource_id) is str:
+            resource_id = ObjectId(resource_id)
+        audit_cursor = self._db.nonComplianceAudits.aggrate([
+            {
+                '$match': {
+                    'resource_id': resource_id
+                }
+            }, {
+                '$lookup': {
+                    'from': 'users',
+                    'localField': 'user_id',
+                    'foreignField': '_id',
+                    'as': 'user'
+                }
+            }, {
+                '$unwind': {
+                    'path': '$user'
+                }
+            }, {
+                '$lookup': {
+                    'from': 'rules',
+                    'localField': 'rule_id',
+                    'foreignField': '_id',
+                    'as': 'rule'
+                }
+            }, {
+                '$unwind': {
+                    'path': '$rule'
+                }
+            }, {
+                '$lookup': {
+                    'from': 'resourceTypes',
+                    'localField': 'rule.type_id',
+                    'foreignField': '_id',
+                    'as': 'rule.resource_type'
+                }
+            }, {
+                '$unwind': {
+                    'path': '$rule.resource_type'
+                }
+            }, {
+                '$lookup': {
+                    'from': 'resources',
+                    'localField': 'resource_id',
+                    'foreignField': '_id',
+                    'as': 'resource'
+                }
+            }, {
+                '$unwind': {
+                    'path': '$resource'
+                }
+            }, {
+                '$lookup': {
+                    'from': 'accounts',
+                    'localField': 'resource.account_id',
+                    'foreignField': '_id',
+                    'as': 'resource.account'
+                }
+            }, {
+                '$unwind': {
+                    'path': '$resource.account'
+                }
+            }, {
+                '$lookup': {
+                    'from': 'platforms',
+                    'localField': 'resource.account.platform_id',
+                    'foreignField': '_id',
+                    'as': 'resource.account.platform'
+                }
+            }, {
+                '$unwind': {
+                    'path': '$resource.account.platform'
+                }
+            }, {
+                '$lookup': {
+                    'from': 'customers',
+                    'localField': 'resource.account.customer_id',
+                    'foreignField': '_id',
+                    'as': 'resource.account.customer'
+                }
+            }, {
+                '$unwind': {
+                    'path': '$resource.account.customer'
+                }
+            }, {
+                '$lookup': {
+                    'from': 'resourceTypes',
+                    'localField': 'resource.type_id',
+                    'foreignField': '_id',
+                    'as': 'resource.resource_type'
+                }
+            }, {
+                '$unwind': {
+                    'path': '$resource.resource_type'
+                }
+            }
+        ])
+        async for audit in audit_cursor:
+            # TODO Create pipeline model and impliment
+            pass
+    except InvalidId:
+        return []
--- app/models/nonComplianceAudit.py
+++ app/models/nonComplianceAudit.py
@@ -1,11 +1,24 @@
 from datetime import datetime
 from pydantic import Field, BaseModel
 
+from .resources import Resource
+from .rule import Rule
+from .user import ExceptionUser
+
 
 class NonComplianceAudit(BaseModel):
     id: str = Field(description="Non-compliance audit's ID")
     resource_id: str = Field(description="Resource's ID")
     rule_id: str = Field(description="Rule's ID")
     user_id: str = Field(description="User's ID")
+    action: str = Field(description="Action's ID")
+    action_datetime: datetime = Field(description="Action's datetime")
+
+
+class NonComplianceAuditV2(BaseModel):
+    id: str = Field(description="Non-compliance audit's ID")
+    resource: Resource = Field(description="Resource")
+    rule: Rule = Field(description="Rule")
+    user: ExceptionUser = Field(description="User's ID")
     action: str = Field(description="Action's ID")
     action_datetime: datetime = Field(description="Action's datetime")
